b23136ba0ccb34079f379c35ac8986e2b2a78be2
Wiyumiyu <vjuliorc@gmail.com>
2026-04-01 19:30:37 -0600

    Agregar

---
+++ SIGSAC.WEB/Web/Pages/Bautismos/Agregar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Bautismos
+{
+    public class AgregarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
